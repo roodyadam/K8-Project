9512ea63517a3733d703ec657e3d808f4aaf50e1
Roody <roodylovesetihad@gmail.com>
2025-12-01 20:31:25 +0000

    polished project

--- terraform/modules/nginx-ingress/main.tf
+++ terraform/modules/nginx-ingress/main.tf
@@ -16,7 +16,7 @@ resource "helm_release" "nginx_ingress" {
 
   create_namespace = true
   wait             = true
-  timeout          = 900
+  timeout          = 1200
 
   values = [
     file("${path.root}/../kubernetes/nginx-ingress/helm-values.yaml")

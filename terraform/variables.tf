d204771b2e8114eb43ccd786c8b1aac4bcd60a3e
Roody <roodylovesetihad@gmail.com>
2025-12-01 21:51:36 +0000

    Cleanup: Remove local state file and update gitignore [skip ci]

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -49,7 +49,7 @@ variable "single_nat_gateway" {
 variable "kubernetes_version" {
   description = "Kubernetes version for EKS cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.34"
 }
 
 variable "node_instance_types" {
